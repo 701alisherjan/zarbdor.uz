aa9807f8a3337a276c12e640627643aacb710450
701alisherjan <701alisherjan@gmail.com>
2025-11-13 16:46:39 +0500

    app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/pages/navbar/Navbar";
+import Footer from "./components/pages/footer/Footer";
+import Hero from "./components/pages/hero/Hero";
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+      </Routes>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
